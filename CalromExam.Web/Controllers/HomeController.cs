e8d7130a71b65898fc6d1c97ccc03f5d32ad9f29
andoniripollpro <andoni.ripoll.pro@gmail.com>
2014-10-31 16:50:15 +0100

    Cleaning up

---
+++ CalromExam.Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using CalromExam.DAL.Repositiries;
+using CalromExam.Models.Flight;
+using System.Web.Mvc;
+
+namespace CalromExam.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ CalromExam.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CalromExam.Web.MvcApplication" Language="C#" %>
