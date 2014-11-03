8ac71e731e297a8d4b8d64034b9a460a3d4750d1
andoniripollpro <andoni.ripoll.pro@gmail.com>
2014-11-03 18:38:59 +0100

    Partial for widget

---
+++ CalromExam.Web/Controllers/MembershipWidgetController.cs
@@ -0,0 +1,20 @@
+﻿using CalromExam.DAL.Repositiries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalromExam.Web.Controllers
+{
+    public class MembershipWidgetController : Controller
+    {
+        public ActionResult Index()
+        {            
+            //TODO: Iversion of control using Ninject
+            MembershipRepository membershipRepository = new MembershipRepository();
+
+            return View(membershipRepository.MembershipEntities);
+        }
+    }
+}
